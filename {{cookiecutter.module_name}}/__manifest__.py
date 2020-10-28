289ef08b4786c244fc45abccc35ed2778358cd76
Malek Abu Shabab <malikabushabab@yahoo.com>
2020-10-28 04:35:32 +0300

    first commit

---
+++ {{cookiecutter.module_name}}/__manifest__.py
@@ -0,0 +1,19 @@
+{
+
+    "name": "Module name",
+    "version" : "{{cookiecutter.module_version}}.{{cookiecutter.version}}",
+    "summary": "{{cookiecutter.module_short_description}}",
+    "category": "{{cookiecutter.category}}",
+    "description": """ """ ,
+    "depends" : ["base"],
+    "data": [],
+    "license": "{{ cookiecutter.open_source_license }}",
+    "author": "{{cookiecutter.author}}",
+    "website": "{{cookiecutter.website}}",
+    "installable": "{{cookiecutter.installable}}",
+    "application": "{{cookiecutter.is_application}}",
+    "auto_install": False,
+    "images":[],
+    "live_test_url":'',
+
+}
